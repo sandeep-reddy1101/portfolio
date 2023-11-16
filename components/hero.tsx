33379c1a3509497e2e3b8e8df37fba0795ac40cd
sandeep-sandy-git <sandeep.x.sandy49@gmail.com>
2023-11-16 16:57:05 -0600

    added metadata

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,8 +1,4 @@
-import Link from "next/link";
-
-import { socialMedia } from "@/lib/socialMedia";
 import { heroInfo } from "@/lib/data";
-import { theme } from "@/lib/theme";
 
 import HeroContent from "./hero-content";
 import HeroContainer from "./hero-container";
@@ -11,23 +7,6 @@ export default function Hero() {
   return (
     <HeroContainer id="home">
       <HeroContent {...heroInfo} home={true} />
-      <div
-        className={`hidden md:block absolute top-1/2 left-0 transform -translate-y-1/2 ${theme.social.hoverEffect} p-2 ${theme.social.backgroundColor} rounded`}
-      >
-        <div className="w-12">
-          {socialMedia.map((item) => {
-            return (
-              <Link
-                key={item.name}
-                href={item.link}
-                className={`w-full block p-3 transition duration-300 rounded-md ${theme.social.hoverIconBackgroundColor} ${theme.social.hoverIconTextColor}`}
-              >
-                {item.icon}
-              </Link>
-            );
-          })}
-        </div>
-      </div>
     </HeroContainer>
   );
 }
